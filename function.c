696e83d17495393636d39b8b6dbd6e785a7fea72
renaud <renaud.yoaganathan@efrei.net>
2024-04-16 15:38:34 +0200

    init

--- function.c
+++ function.c
@@ -1,2 +1,10 @@
 #include "function.h"
 
+int insert_value(COLUMN *column, int value) {
+    if (column->Tlog == column->Tphys) {
+        column->donnee *= 2;
+        column->donnee = realloc(column->values, column->capacity * sizeof(int));
+    }
+    column->values[column->size++] = value;
+    return 0;
+}
