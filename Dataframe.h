c17d55062b423fbe6d3d1eae7e5d96fa2f56c4cd
Renaud <renaud.yoganathan@efrei.net>
2024-04-21 14:32:19 +0200

    Partie 1 fini

--- Dataframe.h
+++ Dataframe.h
@@ -10,3 +10,32 @@ int insert_value(COLUMN *column, int value);
 void delete_column(COLUMN *column);
 
 void print_column(COLUMN *column);
+
+int value_occurence(COLUMN *column, int value);
+
+int value_position(COLUMN *column, int value);
+
+int value_up_to(COLUMN *column, int value);
+
+int value_down_to(COLUMN *column, int value);
+
+int value_equal_to(COLUMN *column, int value);
+
+void add_line(COLUMN *column, int line, int value);
+
+void delete_line(COLUMN *column, int line , int value);
+
+void add_column(COLUMN *column, char* title);
+
+void rename_column(COLUMN *column, char* title);
+
+int value_exist(COLUMN *column, int value);
+
+int replace_value(COLUMN *column, int line, int value, int new_value);
+
+void print_column_name(COLUMN *column);
+
+int number_of_lines(COLUMN *column);
+
+int number_of_columns(COLUMN *column);
+
--- main.c
+++ main.c
@@ -14,5 +14,52 @@ int main() {
     insert_value(mycolumn, 30);
     print_column(mycolumn);
 
+    printf("Supprimons la la colone mycolumn\n");
+    delete_column(mycolumn);
+
+    printf("Combiens de fois se trouve la valeur 10 dans la colonne mycolumn ?\n");
+    printf("La valeur 10 se trouve %d fois dans la colonne mycolumn\n", value_occurence(mycolumn, 10));
+
+    printf("A quelle position se trouve la valeur 20 dans la colonne mycolumn ?\n");
+    printf("La valeur 20 se trouve à la position %d dans la colonne mycolumn\n", value_position(mycolumn, 20));
+
+    printf("Combien de valeurs sont inférieures à 20 dans la colonne mycolumn ?\n");
+    printf("Il y a %d valeurs inférieures à 20 dans la colonne mycolumn\n", value_up_to(mycolumn, 20));
+
+    printf("Combien de valeurs sont supérieures à 20 dans la colonne mycolumn ?\n");
+    printf("Il y a %d valeurs supérieures à 20 dans la colonne mycolumn\n", value_down_to(mycolumn, 20));
+
+    printf("Combien de valeurs sont égales à 20 dans la colonne mycolumn ?\n");
+    printf("Il y a %d valeurs égales à 20 dans la colonne mycolumn\n", value_equal_to(mycolumn, 20));
+
+    printf("Ajoutons la valeur 35 à la ligne 2\n");
+    add_line(mycolumn, 2, 35);
+
+    printf("Supprimons la valeur 35 à la ligne 2\n");
+    delete_line(mycolumn, 2, 35);
+
+    printf("Ajoutons une colonne à la colonne mycolumn\n");
+    add_column(mycolumn, "mycolumn2");
+
+    printf("Renommons la colonne mycolumn2 en mycolumn3\n");
+    rename_column(mycolumn, "mycolumn3");
+
+    printf("La valeur 10 existe-t-elle dans la colonne mycolumn ?\n");
+    printf("La valeur 10 %s dans la colonne mycolumn\n", value_exist(mycolumn, 10) ? "existe" : "n'existe pas");
+
+    printf("Remplaçons la valeur 10 par 5 à la ligne 0\n");
+    replace_value(mycolumn, 0, 10, 5);
+
+    printf("Le nom de la colonne est : ");
+    print_column_name(mycolumn);
+
+    printf("Le nombre de lignes est : %d\n", number_of_lines(mycolumn));
+
+    printf("Le nombre de colonnes est : %d\n", number_of_columns(mycolumn));
+
+
+
     return 0;
+
+
 }
